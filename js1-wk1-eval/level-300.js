f6f50c96f42f7be70bb1f1807119ed7c697c0e60
ElizabethTBL <elizabethtabala@gmail.com>
2025-02-01 12:45:20 +0000

    level300

--- js1-wk1-eval/level-300.js
+++ js1-wk1-eval/level-300.js
@@ -7,6 +7,9 @@ const time = "21:49";
 
 const macTimeDisplay = `${day.slice(0,3)} ${date} ${month.slice(0,3)} ${time}`;
 
+console.log (macTimeDisplay);
+
+
 // Before running code, answer the following - write down your answers:
 // a) how many function calls are there in this file?
 // b) predict and explain what macTimeDisplay will evaluate to. You can use documentation to look up slice
